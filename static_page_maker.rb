cb8a04fdea520dc89193ac11d5de675a551a091f
Allen Garvey <allengarvey@hotmail.com>
2015-09-25 16:46:35 -0400

    fix bug where page_urls was not global variable

--- static_page_maker.rb
+++ static_page_maker.rb
@@ -4,29 +4,24 @@
 
 require 'fileutils'
 require 'nokogiri'
+require 'open-uri'
 load 'ag_url.rb'
 
-TOP_LEVEL_URL = AG_URL::Url.new "http://www.allengarvey.co"
+TOP_LEVEL_URL = AG_URL::Url.new "http://allengarvey.com"
 OUTPUT_DIR = "~/Documents/" #note trailing slash
 FILE_EXTENSION = '.php' #.html or .php
 FILE_EXTENSIONS = ['.php', '.html', '/'] #file extensions to staticize
 DEFAULT_FILE_NAME = 'index' #used for files with out names, such as dir root '/'
 
-page_urls = []
-build_url_list()
-
-page_urls.each do |url|
-	save_page_files(url)
-end
+$page_urls = []
 
 #accepts argument of type AG_URL::Url
 def build_url_list(url)
 	page = Nokogiri::HTML(open(url.url))
-	page_urls.push url
-	
+	$page_urls.push url
 	page.css('a').each do |a_tag|
 		new_url = AG_URL::Url.new a_tag['href']
-		unless page_urls.include?(new_url) or new_url.tld != TOP_LEVEL_URL.tld or !FILE_EXTENSION.include?(new_url.file_extension)
+		unless $page_urls.include?(new_url) or new_url.tld != TOP_LEVEL_URL.tld or !FILE_EXTENSIONS.include?(new_url.file_extension)
 			build_url_list(new_url)
 		end
 	end
@@ -36,7 +31,7 @@ end
 def save_page_files(url)
 	page = Nokogiri::HTML(open(url.url))
 	path = url.relative_path
-	if path ~= /\/$/
+	if path =~ /\/$/
 		path += DEFAULT_FILE_NAME
 	end
 	full_file_name = OUTPUT_DIR + url.relative_path + FILE_EXTENSION
@@ -45,6 +40,12 @@ def save_page_files(url)
 end
 
 
+build_url_list(TOP_LEVEL_URL)
+puts $page_urls
+
+$page_urls.each do |url|
+	save_page_files(url)
+end
 
 
 
